9f3777b929418f757685d46a362ee42dceb4a16e
busybee <katena.maslova.03@mail.ru>
2023-03-20 08:37:20 +0300

    add new buildings, change width

---
+++ src/ru/vsu/cs/maslovaei/Drawable.java
@@ -0,0 +1,2 @@
+package ru.vsu.cs.maslovaei;public interface Drawable {
+}
--- src/ru/vsu/cs/maslovaei/elements/Building.java
+++ src/ru/vsu/cs/maslovaei/elements/Building.java
@@ -1,8 +1,10 @@
 package ru.vsu.cs.maslovaei.elements;
 
+import ru.vsu.cs.maslovaei.Drawable;
+
 import java.awt.*;
 
-public class Building {
+public class Building implements Drawable {
     public final int x, y, width, height;
 
     public Building(int x, int y, int width, int height) {
@@ -12,19 +14,19 @@ public class Building {
         this.height = height;
     }
 
-    public void drawBuilding(Graphics2D g) {
+    public void draw(Graphics2D g) {
         Color oldColor = g.getColor();
         g.setColor(Color.black);
         g.fillRect(x - width / 2, y - height / 2, width, height);
         int m = height / 100;
         g.setColor(Color.yellow);
         for (int i = 1; i < 3; i++) {
-                g.fillRect((int) (x - Math.pow(-1,i)*width / 4), (int) (y - Math.pow(-1,i)*height / 4), 5, 10);
-            }
+            g.fillRect((int) (x - Math.pow(-1, i) * width / 4), (int) (y - Math.pow(-1, i) * height / 4), 5, 10);
+        }
 
         for (int i = 1; i < 3; i++) {
-                g.fillRect((int) (x - Math.pow(-1,i)*width / 4), (int) (y + Math.pow(-1,i)*height / 4), 5, 10);
-            }
+            g.fillRect((int) (x - Math.pow(-1, i) * width / 4), (int) (y + Math.pow(-1, i) * height / 4), 5, 10);
+        }
         g.setColor(oldColor);
     }
 }
